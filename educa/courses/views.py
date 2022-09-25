f7ca1530a3026f960170722929abcc44fcd729eb
dobauron <safetysafetystrazak@gmail.com>
2022-09-25 09:04:40 +0200

    Add templates for CRUD courses, permissions for views

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic.list import ListView
+from .models import Course
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+
+# class ManageCourseListView(ListView):
+#     model = Course
+#     template_name = 'courses/manage/course/list.html'
+#
+#     def get_queryset(self):
+#         qs = super(ManageCourseListView, self).get_queryset()
+#         return qs.filter(owner=self.request.user)
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+
+class CourseCreateView(OwnerCourseEditMixin,
+                       CreateView,
+                       PermissionRequiredMixin):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(OwnerCourseEditMixin,
+                       UpdateView,
+                       PermissionRequiredMixin):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(OwnerCourseMixin,
+                       DeleteView,
+                       PermissionRequiredMixin):
+    template_name = 'courses/manage/course/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+    permission_required = 'courses.delete_course'
--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'educa',
+        'USER': 'dob',
+        'PASSWORD': 'penpentioli',
     }
 }
 
