88929591c2d6c43a1ab41f975d8bc0cd9998dcc9
dobauron <safetysafetystrazak@gmail.com>
2022-09-26 14:11:30 +0200

    Implementing CMS for module

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -3,17 +3,12 @@ from django.views.generic.list import ListView
 from .models import Course
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
 
 
 # Create your views here.
-
-# class ManageCourseListView(ListView):
-#     model = Course
-#     template_name = 'courses/manage/course/list.html'
-#
-#     def get_queryset(self):
-#         qs = super(ManageCourseListView, self).get_queryset()
-#         return qs.filter(owner=self.request.user)
 
 class OwnerMixin(object):
     def get_queryset(self):
@@ -62,3 +57,30 @@ class CourseDeleteView(OwnerCourseMixin,
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
     permission_required = 'courses.delete_course'
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+                                        id=pk,
+                                        pwner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset=self.get_formset()
+        return self.render_to_response({'course':self.course,
+                                        'formset':formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
